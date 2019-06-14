f82ef312511fd7744d0aa21da9dba4ce81d03815
Roman Hotsiy <gotsijroman@gmail.com>
2019-06-14 12:41:25 +0300

    Fix lint issues

--- bin/swagger-repo.js
+++ bin/swagger-repo.js
@@ -3,7 +3,6 @@
 
 const fs = require('fs-extra');
 const path = require('path');
-const _ = require('lodash');
 const program = require('commander');
 const express = require('express');
 const cors = require('cors');
